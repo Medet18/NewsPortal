7bc14e3c62d813b25a322bd0301146c835d3b175
Medet <medetkurganbaev18@.com>
2022-08-26 14:48:35 +0500

    post -> put

--- routes/api.php
+++ routes/api.php
@@ -47,7 +47,7 @@ Route::group(['middleware' => ['jwt.role:subadmin','jwt.auth'], 'prefix'=>'subad
     Route::get('/news', [NewsController::class, 'index']); 
     Route::get('/news/{id}', [NewsController::class, 'show']); 
     Route::post('/news', [NewsController::class, 'store']); 
-    Route::post('/news/{id}', [NewsController::class, 'update']);
+    Route::put('/news/{id}', [NewsController::class, 'update']);
     Route::delete('/news/{id}', [NewsController::class, 'destroy']);
 
 });
